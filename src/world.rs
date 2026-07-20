586569a6a936b149b0f8a272ba13a7e2cd92482b
Chris Ohk <utilforever@gmail.com>
2026-07-20 12:50:05 +0900

    feat: add graph identifiers and nodes

---
+++ src/world.rs
@@ -0,0 +1,10 @@
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct NodeId(pub usize);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct EdgeId(pub usize);
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Node {
+    pub id: NodeId,
+}
