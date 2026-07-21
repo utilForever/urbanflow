33601bd6e42f2372e095c48877f4716c78c81545
Chris Ohk <utilforever@gmail.com>
2026-07-21 17:51:15 +0900

    test: cover add edge action

--- src/action.rs
+++ src/action.rs
@@ -8,3 +8,26 @@ pub enum Action {
         kind: EdgeKind,
     },
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_edge_uses_world_types() {
+        let action = Action::AddEdge {
+            from: NodeId(1),
+            to: NodeId(2),
+            kind: EdgeKind::Rail,
+        };
+
+        assert_eq!(
+            action,
+            Action::AddEdge {
+                from: NodeId(1),
+                to: NodeId(2),
+                kind: EdgeKind::Rail,
+            }
+        );
+    }
+}
